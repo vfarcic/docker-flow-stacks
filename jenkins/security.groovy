ce9dc7d4ab8abbb733ca4239f854c86ce7fa8721
Viktor <viktor@farcic.com>
2018-04-20 00:25:51 +0200

    Changed Jenkins secrets dir

--- jenkins/security.groovy
+++ jenkins/security.groovy
@@ -6,8 +6,8 @@ import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
 
-def user = new File(env["SECRETS_DIR"] + "/jenkins-user").text.trim()
-def pass = new File(env["SECRETS_DIR"] + "/jenkins-pass").text.trim()
+def user = new File(System.getenv()["SECRETS_DIR"] + "/jenkins-user").text.trim()
+def pass = new File(System.getenv()["SECRETS_DIR"] + "/jenkins-pass").text.trim()
 
 println "Creating user " + user + "..."
 
